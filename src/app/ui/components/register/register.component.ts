b2c76ca4104ade6e8de6824305cbc8c54b2798f3
nurullahicel <nurullahicel@gmail.com>
2024-06-19 15:44:20 +0300

    CreateUser

--- src/app/ui/components/register/register.component.ts
+++ src/app/ui/components/register/register.component.ts
@@ -1,6 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
-import {  User } from '../../../entities/user';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { User } from '../../../entities/user';
+import { UserService } from '../../../services/common/models/user.service';
+import { Create_User } from '../../../contracts/users/create_user';
+
+import {
+  CustomToastrService,
+  ToastrMessageType,
+  ToastrPosition,
+} from '../../../services/ui/custom-toastr.service';
 
 @Component({
   selector: 'app-register',
@@ -8,64 +22,103 @@ import {  User } from '../../../entities/user';
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent implements OnInit {
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private toastrService: CustomToastrService
+  ) {}
   frm: FormGroup;
   ngOnInit(): void {
-    this.frm = this.formBuilder.group({
-      name: [
-        '',
-        [
-          Validators.required,
-          Validators.maxLength(30),
-          Validators.minLength(2)
-        ]
-      ],
-      surname: [
-        '',
-        [
-          Validators.required,
-          Validators.maxLength(30),
-          Validators.minLength(2)
-        ]
-      ],
-      username: ['',
-        [
-          Validators.required,
-          Validators.maxLength(30),
-          Validators.minLength(2)
-        ]],
-      email: ['',
-        [
-          Validators.required,
-          Validators.maxLength(30),
-          Validators.email
-        ]],
-      password: ['',
-        [
-          Validators.required,          
-          // Validators.maxLength(20),
-          // Validators.minLength(8)
-        ]
-      ],
-      confirmPassword: ['',[
-        Validators.required,          
-        // Validators.maxLength(20),
-        // Validators.minLength(8)
-      ]],
-    },{validators : (group : AbstractControl): ValidationErrors | null =>{
-      let pass=group.get("password").value;
-      let confirmPass=group.get("confirmPassword").value;
-      return pass===confirmPass ? null:{notSame: true}
-    }});
+    this.frm = this.formBuilder.group(
+      {
+        name: [
+          '',
+          [
+            Validators.required,
+            Validators.maxLength(30),
+            Validators.minLength(2),
+          ],
+        ],
+        surname: [
+          '',
+          [
+            Validators.required,
+            Validators.maxLength(30),
+            Validators.minLength(2),
+          ],
+        ],
+        username: [
+          '',
+          [
+            Validators.required,
+            Validators.maxLength(30),
+            Validators.minLength(2),
+          ],
+        ],
+        email: [
+          '',
+          [Validators.required, Validators.maxLength(30), Validators.email],
+        ],
+        password: [
+          '',
+          [
+            Validators.required,
+            // Validators.maxLength(20),
+            // Validators.minLength(8)
+          ],
+        ],
+        confirmPassword: [
+          '',
+          [
+            Validators.required,
+            // Validators.maxLength(20),
+            // Validators.minLength(8)
+          ],
+        ],
+      },
+      {
+        validators: (group: AbstractControl): ValidationErrors | null => {
+          let pass = group.get('password').value;
+          let confirmPass = group.get('confirmPassword').value;
+          return pass === confirmPass ? null : { notSame: true };
+        },
+      }
+    );
   }
-  get component(){
+ 
+ 
+  get component() {
     return this.frm.controls;
   }
-submitted:boolean=false;
-  onSubmit(data: User) {
-    this.submitted=true;
-    if(this.frm.invalid)
-        return;
+ 
+  submitted: boolean = false;  
+  async onSubmit(user: User) {
+    this.submitted = true;
+    if (this.frm.invalid) 
+      return;
+
+    const result: Create_User = await this.userService.create(user);
+
+    if (result.succeeded) 
+      {this.toastrService.message(result.message,
+        'Successful User Registration',
+        {
+          messageType: ToastrMessageType.Success,
+          position: ToastrPosition.TopRight,
+        }
+      );}
     
-  }
+    else 
+    {this.toastrService.message(result.message, 
+      'Failed User Registration', 
+      {
+       messageType: ToastrMessageType.Error,
+       position: ToastrPosition.TopRight,
+    });}
+    debugger;
+  
+  
+      
+} 
+ 
 }
